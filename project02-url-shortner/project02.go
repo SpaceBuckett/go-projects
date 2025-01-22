45211594edd3b81c6f2daad6f1a57d832b9ad3c6
SpaceBuckett <shehxadv2.0@gmail.com>
2025-01-22 13:25:30 +0500

    url shortner is ✅

--- project02-url-shortner/project02.go
+++ project02-url-shortner/project02.go
@@ -6,21 +6,26 @@ import (
 )
 
 func main() {
+
 	http.HandleFunc("/", handler)
 	fmt.Println("servers running on port 8080")
 	http.ListenAndServe(":8080", nil)
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path == "/advance" {
-		fmt.Fprintln(w, "🥷")
-	} else {
-		fmt.Fprintln(w, r.URL)
-	}
-
 	urlsMap := map[string]string{
-		"gophercises": "https://gophercises.com",
-		"shazTheDev":  "https://shazTheDev.com",
+		"gophercises":   "https://gophercises.com",
+		"codeasthetics": "https://www.youtube.com/@CodeAesthetic",
 	}
 
+	fmt.Println("Route: ", r.URL.Path[1:])
+	path := r.URL.Path[1:]
+	longUrl, exists := urlsMap[path]
+
+	if exists {
+		http.Redirect(w, r, longUrl, http.StatusFound)
+
+	} else {
+		http.NotFound(w, r)
+	}
 }
